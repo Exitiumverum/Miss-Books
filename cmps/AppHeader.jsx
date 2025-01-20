167ed36da42725847fbf9cf18a4a41fe52b3e44c
Dan Tayari <kisa@tayari.co.il>
2025-01-20 15:38:35 +0200

    //Basic functions work and some css work done#

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -6,7 +6,7 @@ export function AppHeader(){
         <header className="app-header">
                 <section>
                     <h1>Miss Books Shop</h1>
-                    <nav>
+                    <nav className="nav-bar">
                         <NavLink to="/">Home</NavLink>
                         <NavLink to="/about">About</NavLink>
                         <NavLink to="/books">Books</NavLink>
--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -3,7 +3,7 @@ export function BookPreview({ book }) {
 
     const { title, price } = book
     return (
-        <article>
+        <article className="book-container">
             <h2>title: {title}</h2>
             <h4>price: {price}</h4>
             <img src={`../BooksImages/1.jpg`} alt="Book Image"
